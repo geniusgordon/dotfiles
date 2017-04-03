69f99930c6ace25f1c464e65e99476ff4367adb6
Gordon <geniusgordon@gmail.com>
2017-04-04 01:19:43 +0800

    :bug: fix music length

--- polybar/playerctl.sh
+++ polybar/playerctl.sh
@@ -8,6 +8,9 @@ player_status=$(playerctl status 2> /dev/null)
 if [[ $? -eq 0 ]]; then
   metadata="$(playerctl metadata artist) - $(playerctl metadata title)"
 fi
+if [[ ${#metadata} -gt 30 ]]; then
+  metadata=${metadata:0:30}...
+fi
 
 # Foreground color formatting tags are optional
 if [[ $player_status = "Playing" ]]; then
