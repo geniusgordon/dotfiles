316c03c71591c3845d90ffc51cbdb25d893a6cae
Gordon <geniusgordon@gmail.com>
2023-05-16 13:16:20 +0800

    Add accept_word, accept_line

--- stow/.config/nvim/after/plugin/copilot.lua
+++ stow/.config/nvim/after/plugin/copilot.lua
@@ -8,40 +8,66 @@
 -- vim.keymap.set("i", "<C-k>", "<Plug>(copilot-previous)")
 -- vim.keymap.set("i", "<C-l>", "<Plug>(copilot-suggest)")
 --
-require('copilot').setup({
-  suggestion = {
-    enabled = true,
-    auto_trigger = true,
-    debounce = 75,
-    keymap = {
-      accept = false,
-      accept_word = false,
-      accept_line = false,
-      next = "<C-j>",
-      prev = "<C-k>",
-      dismiss = "<C-l>",
-    },
-  },
-  filetypes = {
-    yaml = false,
-    markdown = false,
-    help = false,
-    gitcommit = false,
-    gitrebase = false,
-    hgcommit = false,
-    svn = false,
-    cvs = false,
-    ["."] = false,
-  },
-  copilot_node_command = 'node', -- Node.js version must be > 16.x
-  server_opts_overrides = {},
+require("copilot").setup({
+	suggestion = {
+		enabled = true,
+		auto_trigger = true,
+		debounce = 75,
+		keymap = {
+			accept = false,
+			accept_word = false,
+			accept_line = false,
+			next = "<C-j>",
+			prev = "<C-k>",
+			dismiss = "<C-l>",
+		},
+	},
+	filetypes = {
+		yaml = false,
+		markdown = false,
+		help = false,
+		gitcommit = false,
+		gitrebase = false,
+		hgcommit = false,
+		svn = false,
+		cvs = false,
+		["."] = false,
+	},
+	copilot_node_command = "node", -- Node.js version must be > 16.x
+	server_opts_overrides = {},
 })
 
-vim.keymap.set("i", '<Tab>', function()
+vim.keymap.set("i", "<Tab>", function()
+	if require("copilot.suggestion").is_visible() then
+		require("copilot.suggestion").accept()
+	else
+		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+	end
+end, {
+	silent = true,
+})
+
+vim.keymap.set("i", "<S-Tab>", function()
   if require("copilot.suggestion").is_visible() then
-    require("copilot.suggestion").accept()
+    require("copilot.suggestion").previous()
   else
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<S-Tab>", true, false, true), "n", false)
+  end
+end, {
+  silent = true,
+})
+
+vim.keymap.set("i", "<C-f>", function()
+  if require("copilot.suggestion").is_visible() then
+    require("copilot.suggestion").accept_word()
+  end
+end, {
+  silent = true,
+})
+
+vim.keymap.set("i", "<C-g>", function()
+  if require("copilot.suggestion").is_visible() then
+    require("copilot.suggestion").accept_line()
   end
 end, {
   silent = true,
