f65fece3f2b5358be829b42feb6da9405998816c
Gordon <geniusgordon@gmail.com>
2024-05-28 17:05:22 +0800

    Update treesitter context color

--- stow/.config/nvim/lua/gordon/config/treesitter.lua
+++ stow/.config/nvim/lua/gordon/config/treesitter.lua
@@ -1,75 +1,77 @@
 require("nvim-treesitter.configs").setup({
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  modules = {},
-  ignore_install = {},
-  ensure_installed = {
-    "bash",
-    "c",
-    "cmake",
-    "comment",
-    "cpp",
-    "css",
-    "diff",
-    "dockerfile",
-    "git_rebase",
-    "gitattributes",
-    "gitcommit",
-    "gitignore",
-    "go",
-    "graphql",
-    "html",
-    -- "http",
-    "javascript",
-    "jsdoc",
-    "json",
-    "kotlin",
-    "latex",
-    "lua",
-    "passwd",
-    "python",
-    "ruby",
-    "rust",
-    "solidity",
-    "sql",
-    "svelte",
-    "swift",
-    "terraform",
-    "toml",
-    "tsx",
-    "typescript",
-    "lua",
-    "vim",
-    "vue",
-    "yaml",
-  },
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
-  highlight = {
-    enable = true,
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
+	-- A list of parser names, or "all" (the five listed parsers should always be installed)
+	modules = {},
+	ignore_install = {},
+	ensure_installed = {
+		"bash",
+		"c",
+		"cmake",
+		"comment",
+		"cpp",
+		"css",
+		"diff",
+		"dockerfile",
+		"git_rebase",
+		"gitattributes",
+		"gitcommit",
+		"gitignore",
+		"go",
+		"graphql",
+		"html",
+		-- "http",
+		"javascript",
+		"jsdoc",
+		"json",
+		"kotlin",
+		"latex",
+		"lua",
+		"passwd",
+		"python",
+		"ruby",
+		"rust",
+		"solidity",
+		"sql",
+		"svelte",
+		"swift",
+		"terraform",
+		"toml",
+		"tsx",
+		"typescript",
+		"lua",
+		"vim",
+		"vue",
+		"yaml",
+	},
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	sync_install = false,
+	-- Automatically install missing parsers when entering buffer
+	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+	auto_install = true,
+	highlight = {
+		enable = true,
+		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+		-- Using this option may slow down your editor, and you may see some duplicate highlights.
+		-- Instead of true it can also be a list of languages
+		additional_vim_regex_highlighting = false,
+	},
 })
 
-require('treesitter-context').setup({
-  enable = true,            -- Enable this plugin (Can be enabled/disabled later via commands)
-  max_lines = 3,            -- How many lines the window should span. Values <= 0 mean no limit.
-  min_window_height = 0,    -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-  line_numbers = true,
-  multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
-  trim_scope = 'inner',     -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-  mode = 'cursor',          -- Line used to calculate context. Choices: 'cursor', 'topline'
-  -- Separator between context and content. Should be a single character string, like '-'.
-  -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-  separator = nil,
-  zindex = 20,     -- The Z-index of the context window
-  on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+require("treesitter-context").setup({
+	enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+	max_lines = 3, -- How many lines the window should span. Values <= 0 mean no limit.
+	min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+	line_numbers = true,
+	multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
+	trim_scope = "inner", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+	mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+	-- Separator between context and content. Should be a single character string, like '-'.
+	-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+	separator = nil,
+	zindex = 20, -- The Z-index of the context window
+	on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
 })
 
-vim.api.nvim_set_hl(0, "DiagnosticLineNrHint", { link = "DiagnosticVirtualTextHint" })
+local colors = require("gordon.lib.colors").base_30
+vim.api.nvim_set_hl(0, "TreesitterContext", { bg = colors.one_bg3 })
+vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = false })
