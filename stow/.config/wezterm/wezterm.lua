cce0a8bfc892ee6dc05805dd5524a396aabd3c6f
Gordon <geniusgordon@gmail.com>
2024-05-28 15:56:04 +0800

    Update theme

--- stow/.config/wezterm/wezterm.lua
+++ stow/.config/wezterm/wezterm.lua
@@ -10,8 +10,8 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- config.color_scheme = "Catppuccin Macchiato"
-config.color_scheme = "Tokyo Night"
+config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = "Tokyo Night"
 
 config.font = wezterm.font_with_fallback({
   {
