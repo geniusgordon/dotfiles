8d525bd7848394469b4ec270e4a4a816d46c721c
Gordon <geniusgordon@gmail.com>
2024-06-03 14:54:54 +0800

    Add hrsh7th/cmp-nvim-lsp-signature-help

--- stow/.config/nvim/lua/gordon/config/cmp.lua
+++ stow/.config/nvim/lua/gordon/config/cmp.lua
@@ -25,6 +25,7 @@ cmp.setup({
   sources = {
     { name = "nvim_lua" },
     { name = "nvim_lsp" },
+    { name = 'nvim_lsp_signature_help' },
     { name = "path" },
     { name = "buffer",  keyword_length = 3 },
     { name = "crates" },
--- stow/.config/nvim/lua/gordon/config/lazy.lua
+++ stow/.config/nvim/lua/gordon/config/lazy.lua
@@ -1,133 +1,134 @@
 require("lazy").setup({
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-	},
-	{ "folke/tokyonight.nvim" },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+  },
+  { "folke/tokyonight.nvim" },
 
-	{
-		"ThePrimeagen/harpoon",
-		branch = "harpoon2",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
-	{ "nvim-neotest/nvim-nio" },
-	-- telescope
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = { { "nvim-lua/plenary.nvim" } },
-	},
-	{
-		"nvim-telescope/telescope-fzf-native.nvim",
-		build = "make",
-	},
-	{ "nvim-telescope/telescope-ui-select.nvim" },
-	{ "nvim-telescope/telescope-dap.nvim" },
-	{ "camgraff/telescope-tmux.nvim" },
-	{ "FeiyouG/commander.nvim" },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  { "nvim-neotest/nvim-nio" },
+  -- telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { { "nvim-lua/plenary.nvim" } },
+  },
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+  },
+  { "nvim-telescope/telescope-ui-select.nvim" },
+  { "nvim-telescope/telescope-dap.nvim" },
+  { "camgraff/telescope-tmux.nvim" },
+  { "FeiyouG/commander.nvim" },
 
-	-- treesitter
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-	},
-	{ "nvim-treesitter/nvim-treesitter-context" },
+  -- treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+  },
+  { "nvim-treesitter/nvim-treesitter-context" },
 
-	-- git
-	{ "tpope/vim-fugitive" },
-	{ "sindrets/diffview.nvim" },
-	{ "lewis6991/gitsigns.nvim" },
+  -- git
+  { "tpope/vim-fugitive" },
+  { "sindrets/diffview.nvim" },
+  { "lewis6991/gitsigns.nvim" },
 
-	{ "folke/zen-mode.nvim" },
-	{ "mbbill/undotree" },
+  { "folke/zen-mode.nvim" },
+  { "mbbill/undotree" },
 
-	{ "nvim-tree/nvim-web-devicons" },
-	{ "nvim-lualine/lualine.nvim" },
-	{ "akinsho/bufferline.nvim" },
-	{ "nvim-tree/nvim-tree.lua" },
-	{ "stevearc/oil.nvim" },
-	{ "alexghergh/nvim-tmux-navigation" },
-	{ "zbirenbaum/copilot.lua" },
-	{
-		"folke/todo-comments.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
-	-- { "chrisbra/csv.vim" },
-	{
-		"cameron-wags/rainbow_csv.nvim",
-		config = true,
-		ft = {
-			"csv",
-			"tsv",
-			"csv_semicolon",
-			"csv_whitespace",
-			"csv_pipe",
-			"rfc_csv",
-			"rfc_semicolon",
-		},
-		cmd = {
-			"RainbowDelim",
-			"RainbowDelimSimple",
-			"RainbowDelimQuoted",
-			"RainbowMultiDelim",
-		},
-	},
+  { "nvim-tree/nvim-web-devicons" },
+  { "nvim-lualine/lualine.nvim" },
+  { "akinsho/bufferline.nvim" },
+  { "nvim-tree/nvim-tree.lua" },
+  { "stevearc/oil.nvim" },
+  { "alexghergh/nvim-tmux-navigation" },
+  { "zbirenbaum/copilot.lua" },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  -- { "chrisbra/csv.vim" },
+  {
+    "cameron-wags/rainbow_csv.nvim",
+    config = true,
+    ft = {
+      "csv",
+      "tsv",
+      "csv_semicolon",
+      "csv_whitespace",
+      "csv_pipe",
+      "rfc_csv",
+      "rfc_semicolon",
+    },
+    cmd = {
+      "RainbowDelim",
+      "RainbowDelimSimple",
+      "RainbowDelimQuoted",
+      "RainbowMultiDelim",
+    },
+  },
 
-	-- completion
-	{ "hrsh7th/nvim-cmp" },
-	{ "hrsh7th/cmp-buffer" },
-	{ "hrsh7th/cmp-path" },
-	{ "hrsh7th/cmp-cmdline" },
-	{ "hrsh7th/cmp-nvim-lua" },
-	{ "hrsh7th/cmp-nvim-lsp" },
-	{ "L3MON4D3/LuaSnip" },
-	{ "onsails/lspkind.nvim" },
-	{ "saadparwaiz1/cmp_luasnip" },
-	{
-		"saecki/crates.nvim",
-		tag = "v0.3.0",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
-	{ "zbirenbaum/copilot-cmp" },
-	{ "f3fora/cmp-spell" },
+  -- completion
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-cmdline" },
+  { "hrsh7th/cmp-nvim-lua" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-nvim-lsp-signature-help" },
+  { "L3MON4D3/LuaSnip" },
+  { "onsails/lspkind.nvim" },
+  { "saadparwaiz1/cmp_luasnip" },
+  {
+    "saecki/crates.nvim",
+    tag = "v0.3.0",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  { "zbirenbaum/copilot-cmp" },
+  { "f3fora/cmp-spell" },
   { "ray-x/cmp-treesitter" },
   { "kristijanhusak/vim-dadbod-completion" },
 
-	-- lsp
-	{ "neovim/nvim-lspconfig" },
-	{
-		"williamboman/mason.nvim",
-		build = ":MasonUpdate", -- :MasonUpdate updates registry contents
-	},
-	{ "williamboman/mason-lspconfig.nvim" },
-	{ "folke/neodev.nvim" },
+  -- lsp
+  { "neovim/nvim-lspconfig" },
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate", -- :MasonUpdate updates registry contents
+  },
+  { "williamboman/mason-lspconfig.nvim" },
+  { "folke/neodev.nvim" },
 
-	-- dap
-	{ "mfussenegger/nvim-dap" },
-	{ "jay-babu/mason-nvim-dap.nvim" },
-	{ "rcarriga/nvim-dap-ui" },
+  -- dap
+  { "mfussenegger/nvim-dap" },
+  { "jay-babu/mason-nvim-dap.nvim" },
+  { "rcarriga/nvim-dap-ui" },
 
-	-- format
-	{
-		"jay-babu/mason-null-ls.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"williamboman/mason.nvim",
-			"jose-elias-alvarez/null-ls.nvim",
-		},
-	},
+  -- format
+  {
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "jose-elias-alvarez/null-ls.nvim",
+    },
+  },
 
-	{ "j-hui/fidget.nvim", tag = "legacy" },
-	{ "folke/which-key.nvim" },
+  { "j-hui/fidget.nvim",   tag = "legacy" },
+  { "folke/which-key.nvim" },
 
-	-- {
-	-- 	"rest-nvim/rest.nvim",
-	-- 	dependencies = { "nvim-lua/plenary.nvim" },
-	-- },
+  -- {
+  --  "rest-nvim/rest.nvim",
+  --  dependencies = { "nvim-lua/plenary.nvim" },
+  -- },
 
-	{
-		"kristijanhusak/vim-dadbod-ui",
-		dependencies = {
-			"tpope/vim-dadbod",
-		},
-	},
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      "tpope/vim-dadbod",
+    },
+  },
 })
