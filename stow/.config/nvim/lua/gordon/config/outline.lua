872f555beca78ac7d252d5b30adf36d3fdc951b2
Gordon <geniusgordon@gmail.com>
2024-06-12 17:56:09 +0800

    feat(outline): customize symbols for outline view
    This commit customizes the symbols used in the outline view. Each symbol type (File, Module, Namespace, etc.) now has a specific icon and highlight color associated with it. This enhances the visual representation and readability of the outline view.

--- stow/.config/nvim/lua/gordon/config/outline.lua
+++ stow/.config/nvim/lua/gordon/config/outline.lua
@@ -1,3 +1,40 @@
-require("outline").setup()
+require("outline").setup({
+  symbols = {
+    icons = {
+      File = { icon = "󰈙", hl = "Identifier" },
+      Module = { icon = "", hl = "Include" },
+      Namespace = { icon = "", hl = "Include" },
+      Package = { icon = "", hl = "Include" },
+      Class = { icon = "󰠱", hl = "Type" },
+      Method = { icon = "󰆧", hl = "Function" },
+      Property = { icon = "󰜢", hl = "Identifier" },
+      Field = { icon = "󰜢", hl = "Identifier" },
+      Constructor = { icon = "", hl = "Special" },
+      Enum = { icon = "", hl = "Type" },
+      Interface = { icon = "", hl = "Type" },
+      Function = { icon = "󰊕", hl = "Function" },
+      Variable = { icon = "󰀫", hl = "Constant" },
+      Constant = { icon = "󰏿", hl = "Constant" },
+      String = { icon = "", hl = "String" },
+      Number = { icon = "󰎠", hl = "Number" },
+      Boolean = { icon = "", hl = "Boolean" },
+      Array = { icon = "", hl = "Constant" },
+      Object = { icon = "⦿", hl = "Type" },
+      Key = { icon = "󰌋", hl = "Type" },
+      Null = { icon = "NULL", hl = "Type" },
+      EnumMember = { icon = "", hl = "Identifier" },
+      Struct = { icon = "󰙅", hl = "Structure" },
+      Event = { icon = "", hl = "Type" },
+      Operator = { icon = "󰆕", hl = "Identifier" },
+      TypeParameter = { icon = "", hl = "Identifier" },
+      Component = { icon = "", hl = "Function" },
+      Fragment = { icon = "", hl = "Constant" },
+      TypeAlias = { icon = "", hl = "Type" },
+      Parameter = { icon = "󰈇", hl = "Identifier" },
+      StaticMethod = { icon = "󰆧", hl = "Function" },
+      Macro = { icon = " ", hl = "Function" },
+    },
+  },
+})
 
 vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
