7085e23e73eec5680c373ed3000689db0da9bfdc
Gordon <geniusgordon@gmail.com>
2024-06-26 13:35:47 +0800

    feat(nvim): enhance tabline configuration in Neovim
    - Added a new configuration for the tabline in Neovim.
    - The tabline now shows the tab name, with a maximum width of 40 characters.
    - The maximum width of the tabs component is now a third of the total columns.
    - The tabline only shows the filename, not the full path.
    - The active tab color does not automatically update to match other components.
    - The tabline does not show a symbol next to the tab name if the file has been modified.
    - Added a function to show a "+" symbol next to the tab name if the buffer is modified.

--- stow/.config/nvim/lua/gordon/config/statusline.lua
+++ stow/.config/nvim/lua/gordon/config/statusline.lua
@@ -64,7 +64,44 @@ require("lualine").setup({
     },
   },
   inactive_sections = {},
-  tabline = {},
+  tabline = {
+    lualine_a = {
+      {
+        "tabs",
+        tab_max_length = 40, -- Maximum width of each tab. The content will be shorten dynamically (example: apple/orange -> a/orange)
+        max_length = vim.o.columns / 3, -- Maximum width of tabs component.
+        -- Note:
+        -- It can also be a function that returns
+        -- the value of `max_length` dynamically.
+        mode = 1, -- 0: Shows tab_nr
+        -- 1: Shows tab_name
+        -- 2: Shows tab_nr + tab_name
+
+        path = 0, -- 0: just shows the filename
+        -- 1: shows the relative path and shorten $HOME to ~
+        -- 2: shows the full path
+        -- 3: shows the full path and shorten $HOME to ~
+
+        -- Automatically updates active tab color to match color of other components (will be overidden if buffers_color is set)
+        use_mode_colors = false,
+        show_modified_status = false, -- Shows a symbol next to the tab name if the file has been modified.
+        fmt = function(name, context)
+          -- Show + if buffer is modified in tab
+          local buflist = vim.fn.tabpagebuflist(context.tabnr)
+          local winnr = vim.fn.tabpagewinnr(context.tabnr)
+          local bufnr = buflist[winnr]
+          local mod = vim.fn.getbufvar(bufnr, "&mod")
+
+          return name .. (mod == 1 and " [+]" or "")
+        end,
+      },
+    },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {},
+  },
   winbar = {},
   inactive_winbar = {},
   extensions = {
