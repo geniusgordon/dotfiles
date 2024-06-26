d971c11af7dfcd2ccd23491516fb61e08444dc78
Gordon <geniusgordon@gmail.com>
2024-06-26 12:44:06 +0800

    feat: Improve Neovim and tmux configurations
    - Add new key mappings in Neovim for opening terminal in horizontal and vertical splits
    - Move timeout settings from `which-key.lua` to `set.lua` in Neovim configuration
    - Set escape-time to 0 in tmux configuration for faster response

--- stow/.config/nvim/lua/gordon/config/set.lua
+++ stow/.config/nvim/lua/gordon/config/set.lua
@@ -12,6 +12,9 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
--- stow/.config/nvim/lua/gordon/config/which-key.lua
+++ stow/.config/nvim/lua/gordon/config/which-key.lua
@@ -1,3 +1,1 @@
-vim.o.timeout = true
-vim.o.timeoutlen = 300
 require("which-key").setup({})
